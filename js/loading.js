c8fb3f7a39f826b6812b260a0955981a7c62d6e3
Walysson Paiva <walyssonpaiva@outlook.com>
2019-12-12 15:57:24 -0300

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", noScroll)
 
 function onLoadEvent(){
 	window.removeEventListener("scroll", noScroll);
-    document.querySelector(".initialBody").style.display = "none";
+    //document.querySelector(".initialBody").style.display = "none"; vou manter o loading por uns instantes pra teste, mexe nao pls
 }
 
 function noScroll(){
